fbefced8938cb1d8c85f8c7911f01f57a2d8300b
Dhruvesh Navghare <dun814@gmail.com>
2024-01-04 12:39:31 +0530

    Update index.js - added loggers

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const generalErrorRoute = require('./route/generalError.route');
 
 const app = express();
 app.use((req,res,next)=>{
+
+    // Logging request details
+    console.log('----------------------------------------');
+    console.log(`[${new Date().toLocaleString()}] Incoming Request:`);
+    console.log(`Method: ${req.method}`);
+    console.log(`URL: ${req.originalUrl}`);
+    console.log('Headers:', req.headers);
+    console.log('Query Parameters:', req.query);
+    console.log('Body:', req.body);
+    console.log('----------------------------------------');
+    
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     next();
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use('/payment/notification',paymentsRoute);
 app.use('/',generalErrorRoute);
